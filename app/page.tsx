720c846fc1e1eac0e64d2dc64e0019c472ce3f38
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-05-29 18:19:02 -0400

    test student dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { StudentDashboard } from "@/components/studentDashboard/StudentDashboard";
 
 export default function Home() {
   return (
-    'hello world'
+    <StudentDashboard />
   );
 }
