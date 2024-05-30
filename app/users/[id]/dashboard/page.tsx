d24452eb407a946c25a5eee6c8414b0b68793107
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-05-30 09:13:43 -0400

    creating new app Page for dashboards to help setup mock of auth

---
+++ app/users/[id]/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { StudentDashboard } from "@/components/studentDashboard/StudentDashboard";
+import { fetchAvailableAppointments, fetchUpcomingAppointments } from "@/lib/api";
+import { AvailableAppointment } from "@/types";
+import { formatDate } from "@/lib/helpers";
+
+export default async function Dashboard({ params }: { params: { userId: string } }) {
+    const userURL = new URL(`${process.env.BACKEND_API_URL}/artworks/${params.userId}`);
+
+    const availableAppointments: AvailableAppointment[] = await fetchAvailableAppointments(formatDate(new Date()), true);
+    console.log("availableAppointments", availableAppointments)
+    return (
+        <StudentDashboard availableAppointments={availableAppointments} />
+    );
+}
