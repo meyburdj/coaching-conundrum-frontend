2c78a63ed8570cac0850b5d043dcff03a756a67b
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-05-30 09:16:20 -0400

    Update component to handle data from backend for studentCalendar and availableDates logic. Replace seed data

--- components/studentDashboard/StudentDashboard.tsx
+++ components/studentDashboard/StudentDashboard.tsx
@@ -4,26 +4,28 @@ import { useState } from "react"
 import { AvailableAppointments } from "./AvailableAppointments"
 import { UpcomingAppointments } from "./BookedAppointments"
 import { UpcomingAppointment, AvailableAppointment } from "@/types"
-export function StudentDashboard() {
+
+export function StudentDashboard({ availableAppointments }) {
     const [date, setDate] = useState<Date | undefined>()
-
-    const highlightDates = [
-        new Date(2024, 4, 15),
-        new Date(2024, 4, 18),
-        new Date(2024, 4, 14),
-    ]
-
-    const availableAppointments: AvailableAppointment[] = [
-        { id: 1, date: "April 15, 2023", time: "10:00 AM - 11:00 AM", coachName: "Tom Stein" },
-        { id: 2, date: "April 16, 2023", time: "2:00 PM - 3:00 PM", coachName: "Brooklyn Sharon" },
-        { id: 3, date: "April 17, 2023", time: "9:00 AM - 10:00 AM", coachName: "Jared Brown" },
-    ]
 
     const upcomingAppointments: UpcomingAppointment[] = [
         { id: 4, date: "April 12, 2023", time: "3:00 PM - 4:00 PM", coachPhoneNumber: "123-456-7890" },
         { id: 5, date: "April 14, 2023", time: "11:00 AM - 12:00 PM", coachPhoneNumber: "234-567-8901" },
         { id: 6, date: "April 18, 2023", time: "2:00 PM - 3:00 PM", coachPhoneNumber: "345-678-9012" },
     ]
+
+    /**Take availableAppointments and find all unique dates so that map can 
+     * signify days with available appointments**/
+    const getUniqueDates = (appointments: AvailableAppointment[]) => {
+        const dateSet = new Set<string>();
+        appointments.forEach((appointment) => {
+            const date = new Date(appointment.start_time).toDateString();
+            dateSet.add(date);
+        });
+        return Array.from(dateSet).map(date => new Date(date));
+    };
+
+    const highlightDates = getUniqueDates(availableAppointments);
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6 md:p-10">
