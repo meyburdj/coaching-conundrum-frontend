bbb832e0fccd34bd4fdfc08910b12c9e2eb8f654
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-05-31 15:49:54 -0400

    add fetch and functionality for server side fetching of bookedAppointments

--- app/users/[id]/dashboard/page.tsx
+++ app/users/[id]/dashboard/page.tsx
@@ -1,14 +1,15 @@
 import { StudentDashboard } from "@/components/studentDashboard/StudentDashboard";
-import { fetchAvailableAppointments, fetchUpcomingAppointments } from "@/lib/api";
-import { AvailableAppointment } from "@/types";
+import { fetchAvailableAppointments, fetchUpcomingAppointmentsFromServer } from "@/lib/api";
+import { AvailableAppointment, UpcomingAppointment } from "@/types";
 import { formatDate } from "@/lib/helpers";
 
-export default async function Dashboard({ params }: { params: { userId: string } }) {
-    const userURL = new URL(`${process.env.BACKEND_API_URL}/artworks/${params.userId}`);
+export default async function Dashboard({ params }: { params: { id: number } }) {
+    const studentId = params.id
 
+    const bookedAppointments: UpcomingAppointment[] = await fetchUpcomingAppointmentsFromServer(studentId);
     const availableAppointments: AvailableAppointment[] = await fetchAvailableAppointments(formatDate(new Date()), true);
     console.log("availableAppointments", availableAppointments)
     return (
-        <StudentDashboard initialAvailableAppointments={availableAppointments} />
+        <StudentDashboard initialAvailableAppointments={availableAppointments} studentId={studentId} initialBookedAppointments={bookedAppointments} />
     );
 }
