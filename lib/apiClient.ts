d595f269516fc34502ae8cfd1edb9103a7f70853
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-05-31 15:53:56 -0400

    add bookAppointmentFromClient

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -8,3 +8,20 @@ export async function fetchAvailableAppointmentsFromClient(selected_time: string
     const data = await response.json();
     return data;
 }
+
+export async function bookAppointmentFromClient(appointmentId: number, studentId: number): Promise<AvailableAppointment> {
+    console.log('params from clien api', studentId, appointmentId)
+    const response = await fetch(`/api/bookappointment`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ student_id: studentId, appointmentId: appointmentId }),
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to book appointment');
+    }
+
+    return response.json();
+}
