1a7339e4bd7505b712760eb72b145295e00f7244
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-05-31 15:53:39 -0400

    Fix env url. Add bookAppointmentFromServer

--- lib/api.ts
+++ lib/api.ts
@@ -12,10 +12,26 @@ export async function fetchAvailableAppointments(selectedTime?: string, availabl
     return response.json();
 }
 
-export async function fetchUpcomingAppointments(studentId: number): Promise<UpcomingAppointment[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/appointments/student/${studentId}`, { next: { revalidate: 0 } });
+export async function fetchUpcomingAppointmentsFromServer(studentId: number): Promise<UpcomingAppointment[]> {
+    const response = await fetch(`${process.env.BACKEND_API_URL}/appointments/student/${studentId}`, { next: { revalidate: 0 } });
     if (!response.ok) {
         throw new Error("Failed to fetch upcoming appointments");
     }
+    return response.json();
+}
+
+export async function bookAppointmentFromServer(appointmentId: number, studentId: number): Promise<AvailableAppointment> {
+    const response = await fetch(`${process.env.BACKEND_API_URL}/appointments/${appointmentId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ student_id: studentId }),
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to book appointment');
+    }
+
     return response.json();
 }
