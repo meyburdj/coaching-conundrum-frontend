16c623e66079f6a625d0d225c6bf3795e9767ef1
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-06-03 15:39:24 -0400

    fix interface syntax. Add CoachAppointment

--- types.ts
+++ types.ts
@@ -1,24 +1,36 @@
 export interface User {
-    id: number
-    name: string
-    phoneNumber: string
-    role: 'student' | 'coach'
+    id: number;
+    name: string;
+    phoneNumber: string;
+    role: 'student' | 'coach';
 }
 
 export interface Appointment {
-    id: number
+    id: number;
     start_time: string;
-    coachId: number
-    studentId?: number
-    coach_name: string
-    phone_number?: string
+    coachId: number;
+    studentId?: number;
+    coach_name: string;
+    phone_number?: string;
+}
+
+export interface CoachAppointment {
+    id: number;
+    start_time: string;
+    coach_id: number;
+    coach_name: string;
+    student_id: number;
+    phone_number: string;
+    student_name: string;
+    review: AppointmentReview;
+
 }
 
 export interface AppointmentReview {
-    id: number
-    appointmentId: number
-    satisfactionScore: number
-    notes: string
+    id: number;
+    appointment_id: number;
+    satisfaction_score: number;
+    notes: string;
 }
 
 export interface User {
