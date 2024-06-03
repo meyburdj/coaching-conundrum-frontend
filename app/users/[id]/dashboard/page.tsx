b34fd7f2bbdd633798f20af06ae1e1b65383f4c4
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2024-06-03 15:42:38 -0400

    update to conditionally render based on mock auth pathway. Resolve all promises together to not io block twice

--- app/users/[id]/dashboard/page.tsx
+++ app/users/[id]/dashboard/page.tsx
@@ -1,15 +1,40 @@
 import { StudentDashboard } from "@/components/studentDashboard/StudentDashboard";
-import { fetchAvailableAppointments, fetchUpcomingAppointmentsFromServer } from "@/lib/api";
-import { AvailableAppointment, UpcomingAppointment } from "@/types";
+import {
+    fetchAvailableAppointments,
+    fetchUpcomingAppointmentsFromServer,
+    fetchCoachAppointmentsFromServer
+} from "@/lib/api";
+import { AvailableAppointment, UpcomingAppointment, CoachAppointment } from "@/types";
 import { formatDate } from "@/lib/helpers";
+import { CoachDashboard } from "@/components/coachDashboard/CoachDashboard";
 
-export default async function Dashboard({ params }: { params: { id: number } }) {
-    const studentId = params.id
+interface SearchParams {
+    role: string;
+}
 
-    const bookedAppointments: UpcomingAppointment[] = await fetchUpcomingAppointmentsFromServer(studentId);
-    const availableAppointments: AvailableAppointment[] = await fetchAvailableAppointments(formatDate(new Date()), true);
-    console.log("availableAppointments", availableAppointments)
-    return (
-        <StudentDashboard initialAvailableAppointments={availableAppointments} studentId={studentId} initialBookedAppointments={bookedAppointments} />
-    );
+export default async function Dashboard({ params, searchParams }: { params: { id: number }, searchParams: SearchParams }) {
+    const userId = params.id;
+    const role = searchParams.role;
+
+    if (role === 'coach') {
+        const initialAppointments: CoachAppointment[] = await fetchCoachAppointmentsFromServer(userId);
+        return (
+            <CoachDashboard
+                initialAppointments={initialAppointments}
+                coach_id={userId}
+            />);
+    } else {
+        const [bookedAppointments, availableAppointments]:
+            [UpcomingAppointment[], AvailableAppointment[]] = await Promise.all([
+                fetchUpcomingAppointmentsFromServer(userId),
+                fetchAvailableAppointments(formatDate(new Date()), true)
+            ]);
+        return (
+            <StudentDashboard
+                initialAvailableAppointments={availableAppointments}
+                studentId={userId}
+                initialBookedAppointments={bookedAppointments}
+            />
+        );
+    }
 }
